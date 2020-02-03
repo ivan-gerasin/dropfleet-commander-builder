22a2aee705933647d637dea1d90fb3cc8b17a172
Ivan Gerasin <ivan@gerasin.dev>
2020-02-03 21:58:16 +0700

    test setup

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    roots: [
+        "<rootDir>",
+    ],
+    modulePaths: [
+        "<rootDir>",
+        "<rootDir>/src",
+        "<rootDir>/test"
+    ],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
