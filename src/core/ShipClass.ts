364c9285d5bd4ebb8cf4b7069079216fe922e86b
Ivan Gerasin <ivan@gerasin.dev>
2020-02-09 17:17:23 +0700

    add ship class enum

---
+++ src/core/ShipClass.ts
@@ -0,0 +1,37 @@
+enum ShipClass {
+	Frigate,
+	Corvette,
+	StrikeCarrier,
+	Destroyer,
+	BlockadeRunner,
+	Monitor,
+	EscortFrigate,
+	MissileFrigate,
+	AegisFrigate,
+	HunterKiller,
+	Barge,
+	VoidGate,
+	HeavyVoidGate,
+	CloudFlier,
+	HomeShip,
+
+
+	LightCruiser,
+	AssaultTroopship,
+	VanguardCarrier,
+	FleetCarrier,
+	Troopship,
+	MotherShip,
+
+	Cruiser,
+
+	HeavyCruiser,
+	BattleCruiser,
+
+	Battleship,
+	Dreadnought,
+	SuperCarrier
+
+}
+
+export default ShipClass
